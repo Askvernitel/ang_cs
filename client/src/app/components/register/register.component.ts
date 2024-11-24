d715096fc886fd9a35c5a5043a01241443c8b0e5
Irakli <iraklichabiev7@gmail.com>
2024-11-24 14:52:54 +0400

    first commit

---
+++ client/src/app/components/register/register.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Job } from 'src/app/interfaces/job';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  registrationForm!: FormGroup;
+  jobOptions: Job[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchJobOptions;
+    this.createForm();
+    this.fetchJobOptions();
+  }
+
+  createForm(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      jobId: [null, Validators.required]
+    });
+  }
+
+  Register(): void {
+    if (this.registrationForm.valid) {
+      const { confirmPassword, jobId, ...userData } = this.registrationForm.value;
+      const job = jobId as Job;
+
+      this.userService.registerUser({ ...userData, jobId: job.id }).subscribe({
+        next: (response) => {
+          console.log("Registration successful: ",response);
+        },
+        error: (error) => {
+          console.log('Registration failed: ', error);
+        }
+      });
+    }
+  }
+
+
+  fetchJobOptions(): void {
+    this.userService.getJobOptions().subscribe({
+      next: (response) => {
+        console.log('Job options:', response);
+        this.jobOptions = response;
+      },
+      error: (error) => {
+        console.error('Error fetching job options:', error);
+      }
+    });
+  }
+
+
+
+  
+}
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Job } from '../interfaces/job';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'https://localhost:44330/api/user';
+
+  constructor(
+    private http: HttpClient,
+    ) { }
+
+  getJobOptions(): Observable<Job[]> {
+    return this.http.get<Job[]>(`${this.apiUrl}/jobs`);
+  }
+
+  registerUser(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+}
